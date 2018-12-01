9f9339643fb40984720f66317356f8881e45131b
gitbike <git_bike_840@yahoo.co.jp>
2018-12-01 21:17:10 +0900

    View関数がルートに応じたビューを返すように書き換え。ハッシュが空文字の時は何もしない（ビューを変化させない）

--- public/app.js
+++ public/app.js
@@ -1,7 +1,18 @@
 'use strict';
 // グローバルのwindowオブジェクトやライブラリとの名前衝突を避けるため、名前空間を作成
 const learnjs = {};
+
+learnjs.problemView = function () {
+  return $('<div class="problem-view">').text('Coming soon!');
+  
+}
+
 learnjs.showView = function (hash) {
-  const problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+  const routes = {
+    '#problem-1': learnjs.problemView
+  };
+  const viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,4 +3,11 @@ describe('LearnJS', function () {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
   });
+
+  // ヌルケース（デフォルトケース）,空の文字列・空の配列・空のオブジェクト・数値0の場合などをテストする
+  // 以下はハッシュが空文字の場合のrouteのテスト
+  it('shows the landing page view when there is no hash', function () {
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
 });
